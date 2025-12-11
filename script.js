7b49c087d27047b30b6a97f7b5f8e9d46ed18168
Amir-wadih <amirwadi12341@gmail.com>
2025-12-11 12:03:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -90,3 +90,24 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+const mybutton = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () {
+    scrollFunction();
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        mybutton.style.display = "block";
+    }else{
+      mybutton.style.display = "none";  
+    }
+}
+ mybutton.addEventListener('click',function() {
+window.scrollTo({
+
+    top:0,
+   behavior:'smooth'
+});    
+ })
